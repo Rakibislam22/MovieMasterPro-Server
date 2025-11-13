38277cfe16b47bf24e0e4d7fe51653c4559c2df2
Rakibislam22 <rakibislam13122018@gmail.com>
2025-11-13 07:35:49 +0600

    Filtering recently added movie and add stats counter

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('movie server is running!');
+  res.send('movie server is running!');
 })
 
 
@@ -36,45 +36,65 @@ async function run() {
     const moviesCollection = db.collection('movies');
 
     // users data post
-    app.post('/users', async (req,res) => {
-        const newUser = req.body;
-        const email = newUser.email;
+    app.post('/users', async (req, res) => {
+      const newUser = req.body;
+      const email = newUser.email;
 
-        const query = {email : email};
-        const userExist = await userCollection.findOne(query);
+      const query = { email: email };
+      const userExist = await userCollection.findOne(query);
 
-        if(userExist){
-            res.send({message: 'User already exist...!'})
-        }else{
-            const result = await userCollection.insertOne(newUser);
-            res.send(result);
-        }
+      if (userExist) {
+        res.send({ message: 'User already exist...!' })
+      } else {
+        const result = await userCollection.insertOne(newUser);
+        res.send(result);
+      }
     })
 
     // All movies
-    app.get('/movies',async(req,res) => {
-       const cursor = moviesCollection.find();
-       const result = await cursor.toArray();
-       res.send(result);
+    app.get('/movies', async (req, res) => {
+      const cursor = moviesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     })
 
-    app.get('/top-rated-movies',async(req,res) => {
-       const cursor = moviesCollection.find({ rating: { $gte: 8.5 } }).limit(5);
-       const result = await cursor.toArray();
-       res.send(result);
+    app.get("/stats", async (req, res) => {
+      try {
+        const usersCount = await userCollection.countDocuments();
+        const moviesCount = await moviesCollection.countDocuments();
+
+        res.send({
+          users: usersCount,
+          movies: moviesCount
+        });
+      } catch (error) {
+        res.status(500).send({ message: "Error fetching stats", error });
+      }
+    });
+
+    app.get('/top-rated-movies', async (req, res) => {
+      const cursor = moviesCollection.find({ rating: { $gte: 8.7 } }).limit(5);
+      const result = await cursor.toArray();
+      res.send(result);
     })
 
-    app.get('/movies/:id', async (req,res) =>{
+    app.get('/recently-added', async (req, res) => {
+      const cursor = moviesCollection.find().sort({ _id: -1 }).limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/movies/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await moviesCollection.findOne(query);
       res.send(result);
     })
 
-    app.get('/hero-movies',async(req,res) =>{
+    app.get('/hero-movies', async (req, res) => {
       const cursor = moviesCollection.find().limit(8);
       const result = await cursor.toArray();
-       res.send(result);
+      res.send(result);
     })
 
 
@@ -95,5 +115,5 @@ run().catch(console.dir);
 
 
 app.listen(port, () => {
-    console.log(`Movie server is running on port ${port}`);
+  console.log(`Movie server is running on port ${port}`);
 })
